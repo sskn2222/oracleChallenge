7d0ac7bb6f2ab74d74c4b1c90b5acbf886ee1174
sskn2222 <94480476+sskn2222@users.noreply.github.com>
2021-11-17 00:57:44 +0530

    Update OracleChallenge.java
    unwanted imports removed

--- OracleChallenge.java
+++ OracleChallenge.java
@@ -2,8 +2,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class OracleChallenge {
 
